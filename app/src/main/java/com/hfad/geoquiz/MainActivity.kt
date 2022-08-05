2786c0c645c9359c235f6b84f7d63d565de91b82
Aleksey <meysonsib200189@mail.ru>
2022-08-05 20:29:03 +0700

    поэксперементировал с жизненным циклом активити | добавил LOG

--- app/src/main/java/com/hfad/geoquiz/MainActivity.kt
+++ app/src/main/java/com/hfad/geoquiz/MainActivity.kt
@@ -2,11 +2,13 @@ package com.hfad.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,13 +40,16 @@ class MainActivity : AppCompatActivity() {
         Question(
             R.string.question_asia,
             true
-        )
+        ),
+        Question(R.string.matematic, true)
     )
     private var currentIndex = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+
         setContentView(R.layout.activity_main)
 
         true_button = findViewById(R.id.true_button)
@@ -69,6 +74,33 @@ class MainActivity : AppCompatActivity() {
         }
         updateQuestion()
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,
+            "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,
+            "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,
+            "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,
+            "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,
+            "onDestroy() called")
+    }
+
 
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
