e783eeaa01ae6281c658b6fe909a139874cc7e0d
Aleksey <meysonsib200189@mail.ru>
2022-08-12 18:36:45 +0700

    передал в intent дополнительные параметры(помимо открытия другой активити)

--- app/src/main/java/com/hfad/geoquiz/CheatActivity.kt
+++ app/src/main/java/com/hfad/geoquiz/CheatActivity.kt
@@ -1,11 +1,53 @@
 package com.hfad.geoquiz
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+private const val EXTRA_ANSWER_IS_TRUE =
+    "com.bignerdranch.android.geoquiz.answer_is_true"
 
 class CheatActivity : AppCompatActivity() {
+
+    private var answerIsTrue = false // - задал по умолч
+
+    // Добавление функциональности подсматривания ответов -->
+    private lateinit var answerTextView: TextView
+    private lateinit var showAnswerButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cheat)
+
+        answerIsTrue =
+            intent.getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false) // - чтения значения из дополнения
+
+        answerTextView = findViewById(R.id.answer_text_view)
+        showAnswerButton = findViewById(R.id.show_answer_button)
+        showAnswerButton.setOnClickListener {
+            val answerText = when {
+                answerIsTrue -> R.string.true_button
+                else -> R.string.false_button
+            }
+            answerTextView.setText(answerText)
+        }
+
+
+    }
+
+    //MainActivity или любому другому коду
+//приложения ненужно знать подробности реализации тех
+//данных, которые CheatActivity ожидает получить в
+//дополнении интента. Эту работу правильнее инкапсулировать в
+//функцию newIntent(...). -->
+    companion object {
+        fun newIntent(packageContext: Context, answerIsTrue: Boolean): Intent {
+            return Intent(packageContext, CheatActivity::class.java).apply {
+                putExtra(EXTRA_ANSWER_IS_TRUE, answerIsTrue)
+            }
+        }
     }
 }
